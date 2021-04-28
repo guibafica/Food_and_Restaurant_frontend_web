d365119972762b10abc6abeec1fdca5153eb320a
Guilherme Bafica <gb.engcomp@hotmail.com>
2021-04-28 09:13:01 -0300

    ADD: Configura estrutura base

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,14 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Food and Restaurant</title>
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400;500&family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet" />
+
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="></script>
+          <script src="../../js/script.js"></script>
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
